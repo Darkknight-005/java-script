7660609d23f22e1d1c5922c067953203ceeb96c1
Dark knight <67749628+Darkknight-005@users.noreply.github.com>
2021-11-28 16:03:07 +0530

    added variable to repo
    added this variable notes on 20 Nov, 2021

---
+++ variables/variables.js
@@ -0,0 +1,60 @@
+// every section have inside this
+// multi-line comment outer = /**/ *************
+
+// *************    section first this declaration area     *************
+
+// first remove multi-line comment outer from line no.8 - no.17
+/* var hello = '-->    hello';
+
+function myFunction(){
+    var greet = '-->    good morning';
+    console.log(greet);
+}
+console.log(hello);
+console.log(myFunction());
+// comment out the below line and see the code will get error
+// console.log(n_umbers);               */
+
+// *************    section second this redeclaration area with scope    *************
+
+// let keyword version redeclaration setion
+
+// first remove multi-line comment outer from line no.21 - no.31
+
+/* var hello = '-->    hello';
+ var hello = '-->   hi'
+function myFunction(){
+    var greet = '-->    good morning';
+    var greet = '-->    good afternoon';
+    console.log(greet);
+}
+console.log(hello);
+console.log(myFunction());              */
+
+// let keyword version update section
+
+// first remove multi-line comment outer from line no.36 - no.49
+
+/*let hello = '-->    hello';
+hello = '-->    hi'                 // this will not through error
+function myFunction(){
+let greet = '-->    good morning';
+greet = '-->    good afternoon';        // this will not through error
+console.log(greet);
+}
+console.log(hello);
+console.log(myFunction());
+console.log(newFunction());         */
+
+// const keyword version update section  
+/*
+const hello = '-->    hello';
+hello = '-->    hi'                 // this will not through error
+function myFunction(){
+    const greet = '-->    good morning';
+    greet = '-->    good afternoon';        // this will not through error
+    console.log(greet);
+}
+console.log(hello);
+console.log(myFunction());
+console.log(newFunction());             */
