2f268658f0333bb04e909491752f320d037004df
Dark knight <67749628+Darkknight-005@users.noreply.github.com>
2021-11-29 20:24:37 +0530

    just checked
    this final assignment operator js file

--- javascript operators/Assignment operators.js
+++ javascript operators/Assignment operators.js
@@ -26,7 +26,10 @@ Logical nullish assignment          x ??=f()                            x ?? (x 
 11. Equation for right shift operators.
     -->     x/2**y
             Means x is divided by 2 with power y
+
+
 */
+
 const fun = false;
 let sep = "\n";
 let x = 10;
@@ -55,13 +58,13 @@ if( fun === true){
 }else{
     x &= 2;
     console.log(`\n10.Bitwise AND assignment`+' \t--> '+x);
-    x ^=2;
+    x ^=3;
     console.log(`\n11.Bitwise XOR assignment `+' \t--> '+x);
     x |=5;
     console.log(`\n12.Bitwise OR assignment`+' \t--> '+x);
-    x ||=1;
-    console.log(`\n13.logical AND assignment`+' \t--> '+x);
-    x &&=6;
+    let y = false || true;
+    console.log(`\n13.logical AND assignment`+' \t--> '+y);
+    x ||=7;
     console.log(`\n14.logical OR assignment`+' \t--> '+x);
 
     const Default = `you will see this if you change global const 'fun'. Ok remember to set as it is true`;
