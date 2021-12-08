13a07aead17069f58b7ac6832a00cc9afdbe037a
Darkknight <gauravmaurya61@yahoo.in>
2021-12-08 23:00:13 +0530

    write after restart

---
+++ javascript operators/String operators.js
@@ -0,0 +1,11 @@
+/*
+string has one operator which is concatnation operator (+)
+this is use for combining two or more string and make them one.
+and this uniting term is consider as concatnation.
+ */
+
+let a = "\nhello",
+    b = " how are",
+    c = " khana ",
+    x = a+b+c;
+console.log(x+"kha ke jana."+"\n\nThis is an example of concatnation.")
---
+++ javascript operators/Unary operators.js
@@ -0,0 +1,6 @@
+/*
+there are three type of operator 
+1."delete" this operator delete object's property.
+2."typeof" this operator tells us what type of value do operand contains
+3."void"
+ */
